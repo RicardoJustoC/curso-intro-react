75b09fa73648d6d3b0d4a108071dba72d81b7626
Ricardo Justo <ricardojustoc@gmail.com>
2022-07-08 17:12:05 -0500

    construyendo mi primer hook

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { AppUI } from "./AppUI";
-const defaultTodos = [
-  { text: "Cortar cebolla", completed: false },
-  { text: "Tomar el curso de intro a react", completed: false },
-  { text: "Enamorar mujeres", completed: false },
-  { text: "Comer saludable", completed: true },
-];
+// const defaultTodos = [
+//   { text: "Cortar cebolla", completed: false },
+//   { text: "Tomar el curso de intro a react", completed: false },
+//   { text: "Enamorar mujeres", completed: false },
+//   { text: "Comer saludable", completed: true },
+// ];
+function useLocalStorage(itemName, initialValue) {
+  
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  if(!localStorageItem){
+    localStorage.setItem(itemName,JSON.stringify(initialValue));
+    parsedItem=[];
+  }else{
+    parsedItem = JSON.parse(localStorageItem);
+  }
+  const [item, setItem]=React.useState(parsedItem);
+  const saveItem = (newItem)=>{
+    localStorage.setItem(itemName,JSON.stringify(newItem));
+    setItem(newItem);//para renderizar la nueva lista de Item
+  };
+  return [item, saveItem];
+}
 function App() {
-  const [todos, setTodos]=React.useState(defaultTodos);
+  const [todos, saveTodo] = useLocalStorage('TODOS_V1',[]);
+  
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter(todo=>todo.completed).length;
@@ -23,19 +41,20 @@ function App() {
       return todoText.includes(searchText);
     });
   }
+
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];//copia inmutable
     newTodos[todoIndex].completed=true;
-    //para renderizar la nueva lista de todos
-    setTodos(newTodos);
+    saveTodo(newTodos);
   };
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];//copia inmutable
     newTodos.splice(todoIndex,1);
-    //para renderizar la nueva lista de todos
-    setTodos(newTodos);
+    
+    saveTodo(newTodos);
   };
   return (
     <AppUI
