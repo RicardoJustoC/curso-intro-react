8a8facca0631f9ec19030c5637fe5b60ef237b3f
Ricardo Justo <ricardojustoc@gmail.com>
2022-07-12 12:30:01 -0500

    Updates

---
+++ precache-manifest.f41baf0bc76b4032887584a607f9bdfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "999da3e6b9d0f5502f05",
+    "url": "/curso-intro-react/static/css/main.b44a0fb3.chunk.css"
+  },
+  {
+    "revision": "999da3e6b9d0f5502f05",
+    "url": "/curso-intro-react/static/js/main.999da3e6.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/curso-intro-react/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "8f31ac8995903ac72b62",
+    "url": "/curso-intro-react/static/js/runtime~main.8f31ac89.js"
+  },
+  {
+    "revision": "1a6f0b798ddd99c179aa051f63ba8bc6",
+    "url": "/curso-intro-react/static/media/check.1a6f0b79.svg"
+  },
+  {
+    "revision": "f6b8972e5a96e13f1e7e5164167ca7cd",
+    "url": "/curso-intro-react/static/media/delete.f6b8972e.svg"
+  },
+  {
+    "revision": "64563cd1bae8b06f5efedf7d0a9380de",
+    "url": "/curso-intro-react/index.html"
+  }
+];
